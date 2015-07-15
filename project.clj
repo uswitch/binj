ed033f46c6926f496df0058f9037cc812625d43e
Paul Ingles <paul@oobaloo.co.uk>
2015-07-15 10:08:42 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject binj "0.1.0-SNAPSHOT"
+  :description "Clojure library to interact with the Bing Ads API"
+  :url "https://github.com/uswitch/binj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ test/binj/core_test.clj
@@ -0,0 +1,7 @@
+(ns binj.core-test
+  (:require [clojure.test :refer :all]
+            [binj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
