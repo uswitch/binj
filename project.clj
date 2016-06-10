954915e6b4d552aaa33795ad088549c4b51de1be
Paul Ingles <paul@oobaloo.co.uk>
2016-06-10 11:11:35 +0100

    update bing lib to 10.4.5

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject binj "0.1.2"
+(defproject binj "0.2.0-SNAPSHOT"
   :description "Clojure library to interact with the Bing Ads API"
   :url "https://github.com/uswitch/binj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.microsoft.bingads/microsoft.bingads "9.3.4"]
+                 [com.microsoft.bingads/microsoft.bingads "10.4.5"]
                  [clj-http-lite "0.2.1"]
                  [clj-time "0.10.0"]
                  [org.clojure/data.csv "0.1.2"]])
